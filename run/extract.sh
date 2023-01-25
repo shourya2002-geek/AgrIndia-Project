6f6befa1696187e431124b524a9fd771654d7c49
Shourya Gupta <75240318+shourya2002-geek@users.noreply.github.com>
2023-01-25 16:43:05 +0530

    Update extract.sh

--- run/extract.sh
+++ run/extract.sh
@@ -1,7 +1,6 @@
-python -u HyperspecAE/src/extract.py
-	-src_dir HyperspecAE/data/data.mat
-	-ckpt HyperspecAE/logs/final_model.pt
-	-save_dir HyperspecAE/imgs/
+python -u HyperspecAE/src/extract.py\
+	-src_dir HyperspecAE/data/data.mat\
+	-ckpt HyperspecAE/logs/final_model.pt\
 	-num_bands 3\
 	-end_members 2\
 	-encoder_type deep\
